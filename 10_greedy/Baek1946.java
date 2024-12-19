f672fbd2a705f2a995f36edc8a5feed1c80e40a8
Sohee-Kim-ln <livre6491@gmail.com>
2024-12-20 00:24:50 +0900

    solved: Greedy 241219

--- 10_greedy/Baek1946.java
+++ 10_greedy/Baek1946.java
@@ -2,7 +2,6 @@ package algo241219;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -17,12 +16,8 @@ public class Baek1946 {
 
 		for (int tc = 0; tc < T; tc++) {
 			int N = Integer.parseInt(bfr.readLine());
-			boolean[] isPoped = new boolean[N];
 
-			PriorityQueue<int[]> pqA = new PriorityQueue<>((o1, o2) -> Integer.compare(o2[1], o1[1]));
-			PriorityQueue<int[]> pqB = new PriorityQueue<>((o1, o2) -> Integer.compare(o2[2], o1[2]));
-
-			int cnt = N;
+			PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[0], o2[0]));
 
 			// 정보 받기
 			StringTokenizer stz;
@@ -31,53 +26,23 @@ public class Baek1946 {
 				int a = Integer.parseInt(stz.nextToken());
 				int b = Integer.parseInt(stz.nextToken());
 
-				pqA.add(new int[] { i, a, b });
-				pqB.add(new int[] { i, a, b });
+				pq.add(new int[] { a, b });
+
 			}
 
-			boolean finished = false;
+			// 서류1등은 무조건 합격
+			int[] now = pq.poll();
+			int minInterview = now[1];
+			int cnt = 1;
 
-			// 소거되는 지원자 없을 때까지 반복
-			while (!finished) {
-//				System.out.println(pqA.toString());
-//				System.out.println(pqB.toString());
-				int[] nowA = pqA.peek();
-				int[] nowB = pqB.peek();
+			for (int i = 1; i < N; i++) {
+				now = pq.poll();
 
-				// 아직 안없앤 A나올때까지 뽑기
-				while (isPoped[nowA[0]]) {
-					pqA.poll();
-					nowA = pqA.peek();
+				// 면접점수가 최대 등수보다 작다면
+				if (minInterview > now[1]) {
+					cnt++;
+					minInterview = now[1];
 				}
-
-				// 아직 안없앤 A나올때까지 뽑기
-				while (isPoped[nowB[0]]) {
-					pqB.poll();
-					nowB = pqB.peek();
-				}
-				
-//				System.out.println(Arrays.toString(nowA));
-//				System.out.println(Arrays.toString(nowB));
-
-				// A쪽 자격검증
-				if (nowA[1] > nowB[1] && nowA[2] > nowB[2]) {
-					cnt--;
-					isPoped[nowA[0]] = true;
-					pqA.poll();
-					continue;
-				}
-
-				// B쪽 자격검증
-				if (nowA[1] < nowB[1] && nowA[2] < nowB[2]) {
-					cnt--;
-					isPoped[nowB[0]] = true;
-					pqB.poll();
-					continue;
-				}
-
-				// 더이상 소거되는 지원자 없으면 종료플래그 켜기
-				finished = true;
-
 			}
 
 			// 결과 저장
